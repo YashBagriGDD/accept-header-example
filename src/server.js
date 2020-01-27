ae385025f4cea70ccb407df0594f36297cb16b43
Yash Bagri (RIT Student) <yxb1236@ad.rit.edu>
2020-01-27 14:50:33 -0500

    handled response for json and xml

--- src/server.js
+++ src/server.js
@@ -5,11 +5,20 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+    '/': responseHandler.getIndex,
+    '/cats': responseHandler.getCats,
+    index: responseHandler.getIndex
 };
 
 const onRequest = (request, response) => {
+    const parseURL = url.parse(request.url);
+    const acceptedTypes = request.headers.accept.split(',');
 
+    if(urlStruct[parseURL.pathname]) {
+        urlStruct[parseURL.pathname](request, response, acceptedTypes);
+    } else {
+        urlStruct.index(request, response);
+    }
 };
 
 http.createServer(onRequest).listen(port);
